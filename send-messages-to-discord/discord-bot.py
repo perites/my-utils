5ae59956f3ef693967ba8d2b65a4f25c6d21b306
perites <111568507+perites@users.noreply.github.com>
2024-07-09 10:23:36 +0200

    Update discord-bot.py

--- send-messages-to-discord/discord-bot.py
+++ send-messages-to-discord/discord-bot.py
@@ -1,15 +1,16 @@
-import asyncio
 import logging
+import asyncio
 
 import discord
 
 
 class DiscordBot:
-    def __init__(self):
+    def __init__(self, discord_bot_token):
         self.discord_bot = None
+        self.discord_bot_token = discord_bot_token
         self.logger = logging.getLogger("discord-bot")
 
-    async def _send_message(self, channel_id, message):
+    async def _send_message_to_channel(self, channel_id, message):
         @self.discord_bot.event
         async def on_ready():
             channel = self.discord_bot.get_channel(channel_id)
@@ -21,9 +22,13 @@ class DiscordBot:
 
             await self.discord_bot.close()
 
-        await self.discord_bot.start(config.DISCORD_BOT_TOKEN)
+        await self.discord_bot.start(self.discord_bot_token)
         await self.discord_bot.http._HTTPClient__session.close()
 
-    def send_message(self, channel_id, message):
-        self.discord_bot = discord.Client(intents=discord.Intents.default())
-        asyncio.run(self._send_message(channel_id, message))
+    def send_message_to_channel(self, channel_id, message):
+        try:
+            self.discord_bot = discord.Client(intents=discord.Intents.default())
+            asyncio.run(self._send_message_to_channel(channel_id, message))
+
+        except Exception:
+            self.logger.exception(f"Error while sending message to {channel_id}, continuing")
